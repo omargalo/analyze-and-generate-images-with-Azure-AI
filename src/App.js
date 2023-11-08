10206751e28ff096d2217393d3b3cf598b62ce2b
Omar García López <omar.garcia@omargl.net>
2023-11-08 16:32:40 -0600

    fix imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import React, { useState } from 'react';
-import axios from 'axios';
 import { analyzeImage } from './azure-image-analysis';
 
 
