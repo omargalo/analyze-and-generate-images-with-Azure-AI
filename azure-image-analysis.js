0e9bedc2d8a287becf201f8d45b6ec5203d4ad41
Omar García López <omar.garcia@omargl.net>
2023-11-08 16:28:19 -0600

    add analyze image

---
+++ azure-image-analysis.js
@@ -0,0 +1,29 @@
+
+const axios = require('axios');
+
+async function analyzeImage(imageUrl) {
+  const subscriptionKey = '9b34ef48ef184bc0b98218f378bf59cc';
+  const endpoint = 'https://cvazureomar.cognitiveservices.azure.com/vision/v4.0/analyze';
+
+  const params = {
+    'visualFeatures': 'Categories,Description,Color',
+    'details': 'Celebrities,Landmarks',
+    'language': 'en'
+  };
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': subscriptionKey
+  };
+
+  const data = {
+    'url': imageUrl
+  };
+
+  try {
+    const response = await axios.post(endpoint, data, { params, headers });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { analyzeImage } from './azure-image-analysis';
 
-// Create a simple GUI with a title "Computer Vision" and input field for typing in a URL, a button "Analyze" and another button "Generate". 
-// The "Analyze" button should display the image and the "Generate" button should display the image with a caption. 
-// The caption should be generated using the Microsoft Computer Vision API. 
-// You can use the API key from the last assignment. 
-// The caption should be displayed on the image with a black background.
 
 function App() {
+  const [imageUrl, setImageUrl] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState(null);
+
+  const handleAnalyzeImage = async () => {
+    setLoading(true);
+    setResult(null);
+    try {
+      const response = await analyzeImage(imageUrl);
+      setResult(response);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <h1>Computer Vision</h1>
-      <input type="text" placeholder="Enter URL here"></input>
-      <br />
-      <button>Analyze</button>
-      <br />
-      <button>Generate</button>
+    <div>
+      <input type="text" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
+      <button onClick={handleAnalyzeImage}>Analyze Image</button>
+      {loading && <div>Loading...</div>}
+      {result && (
+        <div>
+          <h2>Image Analysis Result</h2>
+          <pre>{JSON.stringify(result, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
 }
